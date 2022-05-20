d421c7a4a6710b8969bb27b7e2e939afade0ae1e
Kenny Martinez <kenny.martinez@healthcatalyst.com>
2022-05-20 12:37:34 -0500

    Test hooks

--- src/app/services/giphy.service.ts
+++ src/app/services/giphy.service.ts
@@ -5,17 +5,18 @@ import { environment } from 'src/environments/environment';
 import { GResponse } from '../models/response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GiphyService {
   private apiRoute = environment.giphyApiUrl;
   private apiKey = environment.giphyApiKey;
   private offset_size = 9;
-  
-  constructor(private httpClient: HttpClient) { }
+
+  constructor(private httpClient: HttpClient) {}
 
   getGifs(searchTerm: string, currentPage: number): Observable<GResponse> {
     const offset = (currentPage - 1) * this.offset_size;
-    const url = `${this.apiRoute}?api_key=${this.apiKey}&q=${searchTerm}&limit=${this.offset_size}&offset=${offset}`;return this.httpClient.get<GResponse>(url);
+    const url = `${this.apiRoute}?api_key=${this.apiKey}&q=${searchTerm}&limit=${this.offset_size}&offset=${offset}`;
+    return this.httpClient.get<GResponse>(url);
   }
 }
