9f9f5ebaee5e24f70d07bf716c6fec4939ab9b25
Kenny Martinez <kenny.martinez@healthcatalyst.com>
2022-05-20 12:44:19 -0500

    using async pipe to avoid suscriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { PageChangedEvent } from 'ngx-bootstrap/pagination';
-import { TagModel } from 'ngx-chips/core/tag-model';
+import { from, map, tap } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { Data } from './models/Data';
 import { GResponse } from './models/response';
 import { GiphyService } from './services/giphy.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   searchTerm = '';
-  response: GResponse;
+  response$: Observable<Data[]>;
   totalItems = 0;
   currentPage = 1;
   items: any[];
 
-  constructor(private giphySvc: GiphyService) {
-  }
+  constructor(private giphySvc: GiphyService) {}
 
   performSearch() {
     this.currentPage = 1;
@@ -30,20 +31,20 @@ export class AppComponent {
   }
 
   private loadImagesFromGiphy() {
-    this.giphySvc.getGifs(this.searchTerm, this.currentPage).subscribe(data => {
-      this.response = data;
-      this.totalItems = data.pagination.total_count / data.pagination.count;
-    });
+    this.response$ = this.giphySvc.getGifs(this.searchTerm, this.currentPage).pipe(
+      tap((result) => (this.totalItems = result.pagination.total_count / result.pagination.count)),
+      map((result) => result.data)
+    );
   }
 
   onItemsChanged(items: string[]) {
     if (items?.length <= 0) {
-      this.response = null;
+      this.response$ = from([]);
       this.searchTerm = '';
       return;
     }
 
-    this.searchTerm = items.join(" ");
+    this.searchTerm = items.join(' ');
     this.performSearch();
   }
 }
